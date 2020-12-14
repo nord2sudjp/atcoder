d547dbcf88afa025240b9b57ca5397f0aa91aaca
maka y <nord2sudjp@gmail.com>
2020-12-14 23:00:02 +0900

    auto commit

---
+++ ABC/ABC_170_D - Not Divisible.py
@@ -0,0 +1,26 @@
+N=int(input())
+*A,=sorted(map(int,input().split()))
+
+l=len(A)
+amax=A[-1]
+DP=[0]*(amax+1)
+         
+for a in A:
+
+    if DP[a]!=0:
+        DP[a]+=1 # DP[a]=2Ç≈ÇÊÇ¢
+        continue # continueÇ≈êÿÇÈ
+    #else:
+        #t=a
+        #while t<=amax:
+    for t in range(a,amax+1,a): # forÇóòópÇ∑ÇÈ
+        #print(a, t)
+        DP[t]+=1
+        #t+=a
+            
+cnt=0
+for a in A:
+    if DP[a]==1:
+        #print(a)
+        cnt+=1
+print(cnt)
