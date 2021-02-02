bab265f92baf07e7296f0584ad31c08c7b9de320
maka y <nord2sudjp@gmail.com>
2021-02-03 08:00:01 +0900

    auto commit

--- 素数列挙.py
+++ 素数列挙.py
@@ -34,15 +34,12 @@ def prime_decomposition(n):
         table.append(n)
     return table
     
-    
-def s(l):
-    n=0
-    for k in range(2,l+1):
-        f=0
-        if k%2==0 and k!=2:continue
-        for d in range(2, k//2):
-            if k%d == 0:
-                f+=1
-        if f==0:
-            n+=1
-    return n
+def s(n):
+    l=[]
+    limit=int(n**0.5)+1
+    for i in range(2,limit+1):
+        while n%i==0:
+            l.append(i)
+            n=n//i
+    if n>1:l.append(n)
+    return(l)
