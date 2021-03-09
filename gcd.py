bf066ad3c8ef74f4943ed0df12b89dca268314df
maka y <nord2sudjp@gmail.com>
2021-03-09 20:00:02 +0900

    auto commit

--- gcd.py
+++ gcd.py
@@ -1,3 +1,4 @@
+#
 import math
 from functools import reduce
 
@@ -6,3 +7,26 @@ def gcd(*numbers):
 
 def gcd_list(numbers):
     return reduce(math.gcd, numbers)
+    
+    
+    
+#
+from enum import Enum
+import sys
+import math
+
+BIG_NUM = 2000000000
+MOD = 1000000007
+EPS = 0.000000001
+
+
+def calc_gcd(a,b):
+    if a < b:
+        a,b = b,a
+    if b == 0:
+        return a
+    return calc_gcd(b,a%b)
+
+
+A,B = map(int,input().split())
+print("%d"%(calc_gcd(A,B)))
