26e62557290eeee6a5ff946dcea51b69580f299d
maka y <nord2sudjp@gmail.com>
2021-04-13 14:00:02 +0900

    auto commit

---
+++ 数論/AOJ_Vol0015_NationalBudget.py
@@ -0,0 +1,14 @@
+from decimal import Decimal, getcontext
+getcontext().prec = 101
+
+N=int(input())
+
+
+for _ in range(N):
+    a = Decimal(input())
+    b = Decimal(input())
+    ans=str(a+b)
+    if len(ans)>80:
+        print("overflow")
+    else:
+        print(ans)
