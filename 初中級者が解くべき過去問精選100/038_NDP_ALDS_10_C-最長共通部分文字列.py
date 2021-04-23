d72e40b87a8c7a3ce9d2c4043e59e99c51541abf
maka y <nord2sudjp@gmail.com>
2021-04-23 20:00:02 +0900

    auto commit

--- 初中級者が解くべき過去問精選100/038_NDP_ALDS_10_C-最長共通部分文字列.py
+++ 初中級者が解くべき過去問精選100/038_NDP_ALDS_10_C-最長共通部分文字列.py
@@ -1,5 +1,4 @@
-# Python‚ÅTLE
-# PyPy3‚ÅAC
+# ALDS1_10_C
 
 Q=int(input())
 
@@ -8,10 +7,10 @@ XY=[[list(input()) for _ in range(2)] for _ in range(Q)]
 def solve(s,t):
     N=len(s)
     M=len(t)
-    MAX_N=N+10
-    MAX_M=M+10
+    MAX_N=N+2
+    MAX_M=M+2
 
-    DP=[[0]*(MAX_N) for _ in range(MAX_M)]
+    DP=[[0]*(MAX_M) for _ in range(MAX_N)]
 
     #DP[i+1][j+1] : S[i]T[j]‚É‘Î‚·‚éLCS‚Ì’·‚³
 
@@ -21,7 +20,11 @@ def solve(s,t):
             if s[i]==t[j]:
                 DP[i+1][j+1]=DP[i][j]+1 #dp[i][j]‚Ü‚Å‚Ì’·‚³‚É1‚ð‘«‚µ‚½•¨
             else:
-                DP[i+1][j+1]=max(DP[i][j+1], DP[i+1][j])
+                #DP[i+1][j+1]=max(DP[i][j+1], DP[i+1][j])
+                if DP[i][j+1] < DP[i+1][j]:
+                    DP[i+1][j+1]=DP[i+1][j]
+                else:
+                    DP[i+1][j+1]=DP[i][j+1]
     print(DP[N][M])
 
 for s,t in XY:
