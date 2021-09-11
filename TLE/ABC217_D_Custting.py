6b308845d438215ed1b1795565361f8747ee5e0a
n2sjp <nord2sudjp@gmail.com>
2021-09-11 23:00:02 +0900

    auto commit

---
+++ TLE/ABC217_D_Custting.py
@@ -0,0 +1,13 @@
+import array
+import bisect
+L, Q = map(int, input().split())
+ 
+arr = array.array("i", [0, L])
+ 
+for i in range(Q):
+  c, x = map(int, input().split())
+  if c == 1:
+    bisect.insort(arr, x)
+  else:
+    a = bisect.bisect_left(arr, x)
+    print(arr[a]-arr[a-1])
